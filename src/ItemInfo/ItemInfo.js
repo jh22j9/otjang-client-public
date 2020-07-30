bb4c3a69ab343af8be5657235c505c7ab1e75c13
gyeongwon1275 <rlaruddnjs28@gmail.com>
2020-07-30 22:54:24 +0900

    ♻️ modify rendering item Info
    아이템 정보가 있을 때에만 해당 text 표시하도록 변경

--- src/ItemInfo/ItemInfo.js
+++ src/ItemInfo/ItemInfo.js
@@ -9,6 +9,7 @@ import Season from '../UIcomponents/Season'
 import ExtraOptions from '../UIcomponents/ExtraOptions'
 import EditButton from '../UIcomponents/EditButton';
 import DeleteButton from '../UIcomponents/DeleteButton';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const styles = StyleSheet.create({
     container: {
@@ -19,8 +20,8 @@ const styles = StyleSheet.create({
         paddingVertical: 20,
         paddingHorizontal: 40
     },
-
     text: {
+        paddingVertical: 6,
         fontSize: 18
     }
 })
@@ -28,16 +29,39 @@ const styles = StyleSheet.create({
 function ItemInfo({ route, navigation, temporaryClothing, ClothesActions }) {
 
     const { index } = route.params;
+    var clothing = temporaryClothing.toJS();
+    const category = clothing.category.categoryValue;
+    const type = clothing.type.typeValue;
+    const seasons = convertSeasonsText(clothing.season.seasonArray).join(', ');
+    const { brand, storage, price } = clothing;
+    const buydate = convertBuydateText(clothing.buydate);
 
+    const infoObject = {
+        category: category,
+        type: type,
+        seasons: seasons,
+        brand: brand,
+        storage: storage,
+        price: price,
+        buydate: buydate
+    }
+
+    console.log('infoObject', infoObject)
     function moveToEditItem() {
         navigation.navigate('EditItemContainer', { index: index })
     };
 
     function deleteItem() {
 
-        function deleteAfterValidate() {
-            let targetItem = { index: index, item: temporaryClothing }
-            ClothesActions.removeClothes(targetItem);
+        async function deleteAfterValidate() {
+
+            let token = await AsyncStorage.getItem('TOKEN');
+            token = JSON.parse(token);
+            let sendingClothingToServer = { index: index, token: token, item: temporaryClothing }
+            ClothesActions.removeClothesToServer(sendingClothingToServer);
+
+            /*    let targetItem = { index: index, item: temporaryClothing }
+               ClothesActions.removeClothes(targetItem); */
             navigation.goBack();
         };
 
@@ -53,6 +77,57 @@ function ItemInfo({ route, navigation, temporaryClothing, ClothesActions }) {
         );
     };
 
+    function convertBuydateText(buydate) {
+
+        const year = String(buydate).split('').slice(0, 2).join('');
+        var month = String(buydate).split('').slice(2)
+
+        if (month[0] === '0') {
+            month.shift();
+        }
+        month = month.join('');
+        return `${year}년 ${month}월`
+    }
+
+    function convertSeasonsText(seasons) {
+
+        var convertedSeasons = [];
+
+        if (seasons[0] === 'sp') { convertedSeasons[0] = '봄' }
+
+        if (seasons[1] === 'sm') {
+            convertedSeasons[1] = '여름'
+        }
+
+        if (seasons[2] === 'f') {
+            convertedSeasons[2] = '가을'
+        }
+
+        if (seasons[3] === 'w') {
+            convertedSeasons[3] = '겨울'
+        }
+
+        return convertedSeasons;
+    }
+
+    function renderItemInfoText() {
+
+        return (
+            <View style={styles.textContainer}>
+
+                <Text style={styles.text}>
+                    {`category : ${category}`}
+                </Text>
+
+                {type ? <Text style={styles.text}>{`type : ${type}`}</Text> : <></>}
+                {seasons ? <Text style={styles.text}>{`계절 : ${seasons}`}</Text> : <></>}
+                {price ? <Text style={styles.text}>{`가격 : ${price}`}</Text> : <></>}
+                {storage ? <Text style={styles.text}>{`보관장소 : ${storage}`}</Text> : <></>}
+                {brand ? <Text style={styles.text}>{`brand : ${brand}`}</Text> : <></>}
+                {buydate ? <Text style={styles.text}>{`구매일자 : ${buydate}`}</Text> : <></>}
+            </View>)
+    }
+
     return (
         <View style={{
             flex: 1,
@@ -60,18 +135,10 @@ function ItemInfo({ route, navigation, temporaryClothing, ClothesActions }) {
             alignItems: 'center'
         }}>
             <View style={styles.container}>
-                <ScrollView>
+                <View>
                     <Gallery temporaryClothing={temporaryClothing} ClothesActions={ClothesActions} />
-
-                    <Text style={styles.text}>{temporaryClothing.get('category')}</Text>
-                    <Text style={styles.text}>{temporaryClothing.get('type')}</Text>
-                    <Text style={styles.text}>{temporaryClothing.get('season')}</Text>
-
-                    {temporaryClothing.get('buydate') !== null ? <Text style={styles.text}>{temporaryClothing.get('buydate')}</Text> : <Fragment />}
-                    {temporaryClothing.get('price') !== null ? <Text style={styles.text}>{temporaryClothing.get('price')}</Text> : <Fragment />}
-                    {temporaryClothing.get('brand') !== null ? <Text style={styles.text}>{temporaryClothing.get('brand')}</Text> : <Fragment />}
-                    {temporaryClothing.get('storage') !== null ? <Text style={styles.text}>{temporaryClothing.get('storage')}</Text> : <Fragment />}
-                </ScrollView>
+                    {renderItemInfoText()}
+                </View>
             </View>
 
             <EditButton onPress={moveToEditItem} />
